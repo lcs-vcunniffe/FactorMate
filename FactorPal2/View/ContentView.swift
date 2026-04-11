81a0e85c05979d3186aed1a87faf9c76b29a5272
Veda Cunniffe <vcunniffe@lcs.on.ca>
2026-04-11 13:48:34 -0400

    Used LaTeX to make a mathematical function, and made variables corresponding to each factor. Also made the guesses the text field input, and reformatted some things.

--- FactorPal2/View/ContentView.swift
+++ FactorPal2/View/ContentView.swift
@@ -13,6 +13,16 @@ struct ContentView: View {
     @State var factorGuess1: String = ""
     @State var factorGuess2: String = ""
     
+    @State var factorR = Int.random(in: 1...6)
+    @State var factorS = Int.random(in: 1...6)
+    
+    var coefficientB: Int {
+        return factorR + factorS
+    }
+    var constantC: Int {
+        return factorR * factorS
+    }
+    
     @State var answersChecked: Bool = false
     
     @State var answer1Correct: Bool = false
@@ -24,14 +34,14 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Factor:")
-            HStack {
-                LaTeX("$x^2$")
-            }
+                .multilineTextAlignment(.leading)
+            LaTeX("$x^2+\(coefficientB)x+\(constantC)$")
             HStack {
                 Text("(x+")
-                TextField("", text: <#Binding<String>#>)
+                TextField("", text: $factorGuess1)
+                    .frame(width: 20)
                 Text(")(x+")
-                TextField("", text: <#T##Binding<String>#>)
+                TextField("", text: $factorGuess2)
                 Text(")")
             }
         }
