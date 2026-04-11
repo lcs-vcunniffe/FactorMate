fcc33131f864bf418dcdd54493a9182d67380cf1
Veda Cunniffe <vcunniffe@lcs.on.ca>
2026-04-11 12:25:17 -0400

    Initial Commit

---
+++ FactorPal2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FactorPal2
+//
+//  Created by Veda Niav Cunniffe on 2026-04-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FactorPal2/FactorPal2App.swift
@@ -0,0 +1,17 @@
+//
+//  FactorPal2App.swift
+//  FactorPal2
+//
+//  Created by Veda Niav Cunniffe on 2026-04-11.
+//
+
+import SwiftUI
+
+@main
+struct FactorPal2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
